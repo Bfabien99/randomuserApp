5812893a0e5caa1678808cbcf250f319cb2d45d2
Brou kouadio Stéphane-Fabien <fabienbrou99@gmail.com>
2024-03-26 17:28:57 +0000

    model user

---
+++ lib/model/user.dart
@@ -0,0 +1,23 @@
+class User {
+  final String gender;
+  final String title;
+  final String firstname;
+  final String lastname;
+  final String email;
+  final String phone;
+  final String cell;
+  final String nat;
+  final String picture;
+
+  User(
+      {required this.gender,
+      required this.title,
+      required this.firstname,
+      required this.lastname,
+      required this.phone,
+      required this.email,
+      required this.cell,
+      required this.nat,
+      required this.picture}
+      );
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,51 +3,85 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+import 'package:user_api/model/user.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<dynamic> users = [];
+  List<User> users = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("User API"),
-        backgroundColor: Colors.blue,
-      ),
-      floatingActionButton: FloatingActionButton(onPressed: fetchUsers, child: Icon(Icons.crisis_alert),),
-      body: ListView.builder(itemCount: users.length,itemBuilder: (context, index){
-        final user = users[index];
-        final email = user['email'];
-        final gender = user['gender'];
-        final name = user['name']['title']+' '+user['name']['last']+' '+user['name']['first'];
-        final imgURL = user['picture']['thumbnail'];
-        return ListTile(
-          leading: ClipRRect(child: Image.network(imgURL), borderRadius: BorderRadius.circular(100),),
-          title: Text(name),
-          subtitle: Row(children: [Text('$gender - '), Flexible(child: Text(email))],),
-        );
-      })
-    );
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("User API"),
+          backgroundColor: Colors.blue,
+        ),
+        backgroundColor: Colors.blue[100],
+        floatingActionButton: FloatingActionButton(
+          onPressed: fetchUsers,
+          child: Icon(Icons.crisis_alert),
+        ),
+        body: ListView.builder(
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              final user = users[index];
+              final color = user.gender == 'male' ? Colors.white : Colors.grey[100];
+
+              return Container(
+                margin: EdgeInsets.only(top: 8, bottom: 8),
+                child: ListTile(
+                  tileColor: color,
+                  leading: ClipRRect(
+                    child: Image.network(user.picture),
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  title: Text('${user.title}. ${user.lastname} ${user.firstname}'),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Gender : ${user.gender}'),
+                      Text('Email : ${user.email}'),
+                      Text('Phone : ${user.phone}'),
+                      Text('Cell : ${user.cell}'),
+                      Text('Nat : ${user.nat}'),
+                    ],
+                  ),
+                ),
+              );
+            }));
   }
 
-  void fetchUsers() async{
+  void fetchUsers() async {
     print("fetchUsers called...");
     const url = "https://randomuser.me/api/?results=100";
     final uri = Uri.parse(url);
     final res = await http.get(uri);
     final body = res.body;
-    var json = jsonDecode(body);
+    final json = jsonDecode(body);
     //print(json);
+    final results = json['results'] as List<dynamic>;
+    final transformed = results.map((e) {
+      return User(
+        gender: e['gender'],
+        title: e['name']['title'],
+        firstname: e['name']['first'],
+        lastname: e['name']['last'],
+        email: e['email'],
+        phone: e['phone'],
+        cell: e['cell'],
+        nat: e['nat'],
+        picture: e['picture']['thumbnail'],
+      );
+    }).toList();
     setState(() {
-      users = json['results'];
+      users = transformed;
     });
     print("fetchUsers completed!");
   }
